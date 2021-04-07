c94dbd399b1d4cab31bf18b03cef370566b1e6a0
Synth <synth9283@gmail.com>
2021-04-07 19:08:02 -0400

    Relocated the order of function

--- front/src/include/lexer.h
+++ front/src/include/lexer.h
@@ -12,7 +12,7 @@ lexer_t *lexerInit(char *content);
 void lexerAdvance(lexer_t *lexer);
 void lexerWhiteSpace(lexer_t *lexer);
 token_t *lexerNextToken(lexer_t *lexer);
+token_t *lexerNextWithToken(lexer_t *lexer, token_t *token);
 token_t *lexerGetString(lexer_t *lexer);
 token_t *lexerGetId(lexer_t *lexer);
-token_t *lexerNextWithToken(lexer_t *lexer, token_t *token);
 char *lexerCharToString(lexer_t *lexer);
